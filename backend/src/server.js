871a38a5fc90eb68beceafb1b7349c8d79a87516
S-Patel05 <sohampatel8609@gmail.com>
2026-04-20 20:59:49 +0530

    development testing

--- backend/src/server.js
+++ backend/src/server.js
@@ -30,11 +30,11 @@ app.get("/health", (req, res) => {
 });
 
 // make our app ready for deployment
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get(/.*/, (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../../frontend/dist/index.html"));
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
