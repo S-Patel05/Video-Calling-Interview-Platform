eb3a8f3c768486a06f5eb8dd60c68b5b9581dfbd
S-Patel05 <sohampatel8609@gmail.com>
2026-04-20 17:10:05 +0530

    initilize the repo

---
+++ frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+import { SignedOut, SignedIn, SignInButton, SignOutButton ,UserButton } from "@clerk/clerk-react";
+
+function App() {
+  return (
+    <>
+      <h1>Welcome to the app</h1>
+
+      <SignedOut>
+        <SignInButton mode="modal" >
+          <button className=' '>Login</button>
+        </SignInButton>
+      </SignedOut>
+
+      <SignedIn>
+        <SignOutButton />
+      </SignedIn>
+
+      <UserButton/>
+    </>
+  );
+}
+
+export default App;
